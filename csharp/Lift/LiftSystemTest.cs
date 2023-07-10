f80e43036b10a07a8d7ce70da93b86f817c87794
Clare Sudbery <clareworking@claresudbery.co.uk>
2023-07-10 12:00:00 +0100

    Fix typo

--- csharp/Lift/LiftSystemTest.cs
+++ csharp/Lift/LiftSystemTest.cs
@@ -9,7 +9,7 @@ namespace Lift
     public class LiftSystemTest
     {
         [Fact]
-        public void Initialises_WithTwoFloors_AndSingleLiftgcam _AsSpecified()
+        public void Initialises_WithTwoFloors_AndSingleLift_AsSpecified()
         {
             var liftA = new Lift("A", 0);
             var lifts = new LiftSystem(new List<int>(){0, 1}, new List<Lift>{liftA}, new List<Call>());
