bb75b3fdf1ab0540173c59ad3d034da10b59b82b
Clare Sudbery <clareworking@claresudbery.co.uk>
2023-07-10 12:47:28 +0100

    Add new test for idle lift fulfils request

--- csharp/Lift/LiftSystemTest.cs
+++ csharp/Lift/LiftSystemTest.cs
@@ -14,11 +14,28 @@ namespace Lift
             // Arrange
             var liftA = new Lift("A", 0);
             var lifts = new List<Lift> { liftA };
-            var floors = new List<int>() { 0, 1 };
+            var floors = new List<int> { 0, 1 };
             var calls = new List<Call>();
             
             // Act
             var liftSystem = new LiftSystem(floors, lifts, calls);
+            
+            // Assert
+            Approvals.Verify(new LiftSystemPrinter().Print(liftSystem));
+        }
+        [Fact]
+        public void IdleLift_FulfilsRequest()
+        {
+            // Arrange
+            var liftA = new Lift("A", 0, new List<int>(){1});
+            var lifts = new List<Lift> { liftA };
+            var floors = new List<int> { 0, 1 };
+            var calls = new List<Call>{new Call(1, Direction.Down)};
+            var liftSystem = new LiftSystem(floors, lifts, calls);
+            
+            // Act
+            liftSystem.Tick();
+            liftSystem.Tick();
             
             // Assert
             Approvals.Verify(new LiftSystemPrinter().Print(liftSystem));
