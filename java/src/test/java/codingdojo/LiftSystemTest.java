af04f58bfb6f7941d4650abf653b2f903d1af526
Clare Sudbery <clareworking@claresudbery.co.uk>
2024-03-02 22:28:46 +0000

    Bring up to date with demo1 branch for java

--- java/src/test/java/codingdojo/LiftSystemTest.java
+++ java/src/test/java/codingdojo/LiftSystemTest.java
@@ -4,17 +4,25 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import static org.approvaltests.Approvals.verify;
 
 public class LiftSystemTest {
 
-    // TODO: enable this test and finish writing it
-    // @Test
-    public void doSomething() {
+    @Test
+    public void initialises_withTwoFloors_andSingleLift_asSpecified() {
+        // Arrange
         Lift liftA = new Lift("A", 0);
-        LiftSystem lifts = new LiftSystem(Arrays.asList(0, 1), Collections.singletonList(liftA), Collections.emptyList());
-        lifts.tick();
-        verify(new LiftSystemPrinter().print(lifts));
+        List<Lift> lifts = Collections.singletonList(liftA);
+        List<Integer> floors = Arrays.asList(0, 1);
+        List<Call> calls = Collections.emptyList();
+
+        // Act
+        LiftSystem liftSystem = new LiftSystem(floors, lifts, calls);
+        liftSystem.tick();
+
+        // Assert
+        verify(new LiftSystemPrinter().print(liftSystem));
     }
 }
