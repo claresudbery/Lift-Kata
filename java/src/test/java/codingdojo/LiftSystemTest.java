5056edacd88bb0ababa173e262b73d3736dbe43a
Clare Sudbery <clareworking@claresudbery.co.uk>
2024-03-02 23:04:53 +0000

    Java: Add new failing test idleLift_fulfilsRequest_inTwoTicks

--- java/src/test/java/codingdojo/LiftSystemTest.java
+++ java/src/test/java/codingdojo/LiftSystemTest.java
@@ -20,9 +20,34 @@ public class LiftSystemTest {
 
         // Act
         LiftSystem liftSystem = new LiftSystem(floors, lifts, calls);
-        liftSystem.tick();
 
         // Assert
         verify(new LiftSystemPrinter().print(liftSystem));
+    }
+
+    @Test
+    public void idleLift_fulfilsRequest_inTwoTicks() {
+        // Arrange
+        List<Integer> requests = Arrays.asList(1);
+        Lift liftA = new Lift("A", 0, requests);
+        List<Lift> lifts = Collections.singletonList(liftA);
+        List<Integer> floors = Arrays.asList(0, 1);
+        List<Call> calls = Collections.emptyList();
+        LiftSystem liftSystem = new LiftSystem(floors, lifts, calls);
+        LiftSystemPrinter liftSystemPrinter = new LiftSystemPrinter();
+        String liftSystemOutput = liftSystemPrinter.print(liftSystem);
+
+        // Act
+        liftSystemOutput = tickAndReturnoutput(liftSystem, liftSystemOutput, liftSystemPrinter);
+        liftSystemOutput = tickAndReturnoutput(liftSystem, liftSystemOutput, liftSystemPrinter);
+
+        // Assert
+        verify(liftSystemOutput);
+    }
+
+    private static String tickAndReturnoutput(LiftSystem liftSystem, String liftSystemOutput, LiftSystemPrinter liftSystemPrinter) {
+        liftSystem.tick();
+        liftSystemOutput += "...\n" + liftSystemPrinter.print(liftSystem);
+        return liftSystemOutput;
     }
 }
