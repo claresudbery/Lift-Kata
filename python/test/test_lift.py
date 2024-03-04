c94a2a1d6ea622aad956bd697596ff6fcaf7e7fa
Clare Sudbery <clareworking@claresudbery.co.uk>
2024-03-04 23:43:48 +0000

    Refactor test for clarity

--- python/test/test_lift.py
+++ python/test/test_lift.py
@@ -5,6 +5,11 @@ from lift_printers import print_lifts
 
 
 def test_initialises_with_two_floors_and_single_lift():
+    #Arrange
     liftA = Lift("A", 0)
+
+    #Act
     lifts = LiftSystem(floors=[0, 1], lifts=[liftA])
+
+    #Assert
     verify(print_lifts(lifts))
