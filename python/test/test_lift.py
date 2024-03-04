93321a7c5b6105da9069ac50cfed85b7113525e1
Clare Sudbery <clareworking@claresudbery.co.uk>
2024-03-04 23:45:49 +0000

    Bring Python in line with part 1

--- python/test/test_lift.py
+++ python/test/test_lift.py
@@ -4,9 +4,12 @@ from lift import Lift, LiftSystem
 from lift_printers import print_lifts
 
 
-# TODO: finish writing this test (remove the 'todo' prefix so the test will get run)
-def todo_test_something():
+def test_initialises_with_two_floors_and_single_lift():
+    #Arrange
     liftA = Lift("A", 0)
+
+    #Act
     lifts = LiftSystem(floors=[0, 1], lifts=[liftA])
-    lifts.tick()
+
+    #Assert
     verify(print_lifts(lifts))
