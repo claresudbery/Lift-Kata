5923ac7f20499a00adb2929c2d2b55d64fc54037
Clare Sudbery <clareworking@claresudbery.co.uk>
2024-03-04 23:34:58 +0000

    Get first test working

--- python/test/test_lift.py
+++ python/test/test_lift.py
@@ -5,7 +5,7 @@ from lift_printers import print_lifts
 
 
 # TODO: finish writing this test (remove the 'todo' prefix so the test will get run)
-def todo_test_something():
+def test_initialises_with_two_floors_and_single_lift():
     liftA = Lift("A", 0)
     lifts = LiftSystem(floors=[0, 1], lifts=[liftA])
     lifts.tick()
