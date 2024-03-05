1cde74b880efdd112513eeeead941169918a4953
Clare Sudbery <clareworking@claresudbery.co.uk>
2024-03-05 00:13:29 +0000

    Add test for no requests after one tick, and helper method

--- python/test/test_lift.py
+++ python/test/test_lift.py
@@ -13,3 +13,20 @@ def test_initialises_with_two_floors_and_single_lift():
 
     #Assert
     verify(print_lifts(lifts))
+
+def test_idle_lift_with_no_request_after_tick_stays_still():
+    #Arrange
+    liftA = Lift("A", 0)
+    lift_system = LiftSystem(floors=[0, 1], lifts=[liftA])
+    lift_system_output = print_lifts(lift_system)
+
+    #Act
+    lift_system_output += tick_and_return_output(lift_system)
+
+    #Assert
+    verify(lift_system_output)
+
+
+def tick_and_return_output(lift_system):
+    lift_system.tick()
+    return "...\n" + print_lifts(lift_system)
