6e0778a470b779d85295b9aa3ffaf551c05f645d
Clare Sudbery <clareworking@claresudbery.co.uk>
2024-03-05 00:18:42 +0000

    Idiomatic Python: Rename and reformat

--- python/test/test_lift.py
+++ python/test/test_lift.py
@@ -5,11 +5,11 @@ from lift_printers import print_lifts
 
 
 def test_initialises_with_two_floors_and_single_lift():
-    #Arrange
-    liftA = Lift("A", 0)
+    # Arrange
+    lift_a = Lift("A", 0)
 
-    #Act
-    lifts = LiftSystem(floors=[0, 1], lifts=[liftA])
+    # Act
+    lifts = LiftSystem(floors=[0, 1], lifts=[lift_a])
 
-    #Assert
+    # Assert
     verify(print_lifts(lifts))
