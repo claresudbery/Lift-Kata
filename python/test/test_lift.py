3e9767f687d08e62e3369c1d0e16ca2adccacf0e
Clare Sudbery <clareworking@claresudbery.co.uk>
2024-03-22 13:02:18 +0000

    added extra test

--- python/test/test_lift.py
+++ python/test/test_lift.py
@@ -13,3 +13,14 @@ def test_initialises_with_two_floors_and_single_lift():
 
     # Assert
     verify(print_lifts(lifts))
+
+def test_idle_lift_with_no_request_after_tick_stays_still():
+    # Arrange
+    lift_a = Lift("A", 0)
+    lift_system = LiftSystem(floors=[0, 1], lifts=[lift_a])
+
+    # Act
+    lift_system.tick()
+
+    # Assert
+    verify(print_lifts(lift_system))
